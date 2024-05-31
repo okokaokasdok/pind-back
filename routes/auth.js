cadc24b34f47dcba4f84a38f53d30958a223927d
okokaokasdok <168406842+okokaokasdok@users.noreply.github.com>
2024-05-31 21:42:14 +0300

    Delete routes directory

--- routes/auth.js
+++
@@ -1,6 +0,0 @@
-const authRouter = require("express").Router();
-const { login } = require("../controllers/auth.js");
-
-authRouter.post("/auth/login", login);
-
-module.exports = authRouter;
--- routes/pages.js
+++
@@ -1,8 +0,0 @@
-const pagesRouter = require("express").Router(); 
-const { sendIndex, sendDashboard } = require("../controllers/auth.js");
-const { checkCookiesJWT, checkAuth } = require("../middlewares/auth.js");
-
-pagesRouter.get("/", sendIndex);
-pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard); 
-
-module.exports = pagesRouter;
